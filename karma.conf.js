50bb18e85562371b988c447a6668c34ed4d5d2f0
Han <hankim813@gmail.com>
2016-06-21 01:19:20 -0700

    Modernize test harness; remove Duo support, add Browserify support (#20)

---
+++ karma.conf.js
@@ -0,0 +1,52 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(config) {
+  config.set({
+    files: [
+      // https://segment.com/docs/integrations/optimizely/#with-javascript
+      'http://cdn.optimizely.com/js/170430035.js',
+      'test/**/*.test.js'
+    ],
+
+    browsers: ['PhantomJS'],
+
+    frameworks: ['browserify', 'mocha'],
+
+    reporters: ['spec', 'coverage'],
+
+    preprocessors: {
+      'test/**/*.js': 'browserify'
+    },
+
+    client: {
+      mocha: {
+        grep: process.env.GREP,
+        reporter: 'html',
+        timeout: 10000
+      }
+    },
+
+    browserify: {
+      debug: true,
+      transform: [
+        [
+          'browserify-istanbul',
+          {
+            instrumenterConfig: {
+              embedSource: true
+            }
+          }
+        ]
+      ]
+    },
+
+    coverageReporter: {
+      reporters: [
+        { type: 'text' },
+        { type: 'html' },
+        { type: 'json' }
+      ]
+    }
+  });
+};
